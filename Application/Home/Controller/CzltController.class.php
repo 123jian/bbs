6d87f103cb710f3d8c22e6394f67806d10984638
ylmemail <ylmemail@163.com>
2014-12-20 15:56:24 +0800

    12

--- Application/Home/Controller/CzltController.class.php
+++ Application/Home/Controller/CzltController.class.php
@@ -3,11 +3,13 @@ namespace Home\Controller;
 use Think\Controller;
 class CzltController extends Controller {
    
-	//传智论坛首页
-	public function index(){
-
-            $this->display();
-	}
+    //传智论坛首页
+    public function index(){
+        
+        
+        $this->assign("data",'123');
+        $this->display();
+    }
 
 	
 }
