5b947019461b00bdaad48026ffcc96966433e70c
ylmemail <ylmemail@163.com>
2014-12-20 15:48:26 +0800

    1

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,6 +1,25 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+
+    //数据库配置信息
+    'DB_TYPE'   => 'mysql', // 数据库类型
+    'DB_HOST'   => '192.168.1.217', // 服务器地址
+    'DB_NAME'   => 'bbs', // 数据库名
+    'DB_USER'   => 'root', // 用户名
+    'DB_PWD'    => '', // 密码
+    'DB_PORT'   => 3306, // 端口
+    'DB_PREFIX' => 'bbs_', // 数据库表前缀 
+    'DB_CHARSET'=> 'utf8', // 字符集
+    'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
+
 	'LAYOUT_ON'=>true,
 	'LAYOUT_NAME'=>'layout',
+
+	'MODULE_ALLOW_LIST' => array (
+			'Home',
+			'Admin',
+			'User' 
+	),
+	'DEFAULT_MODULE' => 'Home',  //URL去除home目录
 );
