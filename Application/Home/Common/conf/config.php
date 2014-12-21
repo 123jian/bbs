61922d8bdece5fb01db09398117738be46e702c4
ylmemail <ylmemail@163.com>
2014-12-21 19:49:09 +0800

    12

---
+++ Application/Home/Common/conf/config.php
@@ -0,0 +1,12 @@
+<?php
+        return array(
+	'MODULE_ALLOW_LIST' => array (
+			'Home',
+			'Admin',
+			'User' 
+	),
+	'DEFAULT_MODULE' => 'Home',
+        'LAYOUT_ON'=>true,
+        'LAYOUT_NAME'=>'layout',
+    );
+?>
