87fff6784d5ada06a02bfd9dd3fcfacc7b79000f
ylmemail <ylmemail@163.com>
2014-12-21 19:21:27 +0800

    12

--- Application/Home/Controller/CzltController.class.php
+++ Application/Home/Controller/CzltController.class.php
@@ -18,8 +18,60 @@ class CzltController extends Controller {
     //技术论坛
     public function jslt(){       
         
-
+        $nav = M("nav");
+        $data = $nav->where('nav_category=1 and isset=1')->select();
         $this->assign("nav",$data);
+        
+        $this->display();
+    }
+    
+    //IT问答精灵
+    public function wdjl(){       
+        
+        $nav = M("nav");
+        $data = $nav->where('nav_category=1 and isset=1')->select();
+        $this->assign("nav",$data);
+        
+        $this->display();
+    }
+    
+    //传智特刊
+    public function cztk(){       
+        
+        $nav = M("nav");
+        $data = $nav->where('nav_category=1 and isset=1')->select();
+        $this->assign("nav",$data);
+        
+        $this->display();
+    }
+    
+    //积分商城
+    public function jfsc(){       
+        
+        $nav = M("nav");
+        $data = $nav->where('nav_category=1 and isset=1')->select();
+        $this->assign("nav",$data);
+        
+        $this->display();
+    }
+    
+    //免费公开课
+    public function mfgkk(){       
+        
+        $nav = M("nav");
+        $data = $nav->where('nav_category=1 and isset=1')->select();
+        $this->assign("nav",$data);
+        
+        $this->display();
+    }
+    
+    //积分抽奖
+    public function jfcj(){       
+        
+        $nav = M("nav");
+        $data = $nav->where('nav_category=1 and isset=1')->select();
+        $this->assign("nav",$data);
+        
         $this->display();
     }
 
