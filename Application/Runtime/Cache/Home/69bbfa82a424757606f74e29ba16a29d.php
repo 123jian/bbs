0a704b2786845718857efa427808d59a3fad6fce
ylmemail <ylmemail@163.com>
2014-12-22 17:17:57 +0800

    12

--- Application/Runtime/Cache/Home/69bbfa82a424757606f74e29ba16a29d.php
+++ Application/Runtime/Cache/Home/69bbfa82a424757606f74e29ba16a29d.php
@@ -12,9 +12,12 @@
 <meta name="copyright" content="2001-2013 Comsenz Inc." />
 <meta name="MSSmartTagsPreventParsing" content="True" />
 <meta http-equiv="MSThemeCompatible" content="Yes" />
+
 <base href="http://bbs.itcast.cn/" />
 <link rel="stylesheet" type="text/css" href="data/cache/style_8_common.css?K3L" />
-<link rel="stylesheet" type="text/css" href="data/cache/style_8_forum_index.css?K3L" /><script type="text/javascript">var STYLEID = '8', STATICURL = 'static/', IMGDIR = 'static/image/common', VERHASH = 'K3L', charset = 'utf-8', discuz_uid = '0', cookiepre = 'UwrY_2132_', cookiedomain = '', cookiepath = '/', showusercard = '0', attackevasive = '0', disallowfloat = 'sendpm|newthread|nav', creditnotice = '1|金钱|,2|经验|,3|金币|,4|宣传贡献|,5|冻结金币|', defaultstyle = '', REPORTURL = 'aHR0cDovL2Jicy5pdGNhc3QuY24vZm9ydW0ucGhw', SITEURL = 'http://bbs.itcast.cn/', JSPATH = 'static/js/', CSSPATH = 'data/cache/style_', DYNAMICURL = '';</script>
+<link rel="stylesheet" type="text/css" href="data/cache/style_8_forum_index.css?K3L" />
+<script type="text/javascript">
+var STYLEID = '8', STATICURL = 'static/', IMGDIR = 'static/image/common', VERHASH = 'K3L', charset = 'utf-8', discuz_uid = '0', cookiepre = 'UwrY_2132_', cookiedomain = '', cookiepath = '/', showusercard = '0', attackevasive = '0', disallowfloat = 'sendpm|newthread|nav', creditnotice = '1|金钱|,2|经验|,3|金币|,4|宣传贡献|,5|冻结金币|', defaultstyle = '', REPORTURL = 'aHR0cDovL2Jicy5pdGNhc3QuY24vZm9ydW0ucGhw', SITEURL = 'http://bbs.itcast.cn/', JSPATH = 'static/js/', CSSPATH = 'data/cache/style_', DYNAMICURL = '';</script>
 <script src="static/js/common.js?K3L" type="text/javascript"></script>
 <meta name="application-name" content="传智论坛-IT学习社区" />
 <meta name="msapplication-tooltip" content="传智论坛-IT学习社区" />
@@ -22,6 +25,7 @@
 <meta name="msapplication-task" content="name=技术论坛;action-uri=http://bbs.itcast.cn/forum.php;icon-uri=http://bbs.itcast.cn/static/image/common/bbs.ico" />
 <meta name="msapplication-task" content="name=高校俱乐部;action-uri=http://bbs.itcast.cn/group.php;icon-uri=http://bbs.itcast.cn/static/image/common/group.ico" />
 <script src="static/js/forum.js?K3L" type="text/javascript"></script>
+
 
 <meta http-equiv="X-UA-Compatible" content="IE=8" >
 <meta http-equiv="X-UA-Compatible" content="IE=9" >
@@ -97,13 +101,18 @@ window.onerror = killErrors;
 </div>
  
       
-      <a class="top_login" href="member.php?mod=logging&amp;action=login&amp;referer=" onClick="showWindow('login', this.href);return false;"  title="登录" > 登录 </a> <a class="top_reg"  href="member.php?mod=register"  title="Register" > 注册会员 </a> <a class="top_find_psw" href="javascript:;" onClick="showWindow('login', 'member.php?mod=logging&action=login&viewlostpw=1')"> 找回密码 </a> 
-       
-      
-       
-       
-      
-       
+      <a class="top_login" href="member.php?mod=logging&amp;action=login&amp;referer=" onClick="showWindow('login', this.href);return false;"  title="登录" > 登录 </a> <a class="top_reg"  href="member.php?mod=register"  title="Register" > 注册会员 </a> <a class="top_find_psw" href="javascript:;" onClick="showWindow()"> 找回密码 </a>
+<!--<a class="top_find_psw" href="javascript:;" onClick="showWindow('login', 'member.php?mod=logging&action=login&viewlostpw=1')"> 找回密码 </a>-->
+
+<!--短信找回密码start-->
+<script>
+function showWindow(){
+    location.href="http://www.bbs.com/index.php/czlt/zhmm";
+}
+</script>    
+<!--短信找回密码end-->      
+
+     
     </div>
   </div>
   </div>
@@ -644,7 +653,8 @@ GMT+8, 2014-12-17 09:42<span id="debuginfo">
   <span hidefocus="true"><a title="返回顶部" onclick="window.scrollTo('0','0')" class="scrolltopa" ><b>返回顶部</b></a></span> 
    
 </div>
-<script type="text/javascript">_attachEvent(window, 'scroll', function () { showTopLink(); });checkBlind();</script> 
+<script type="text/javascript">
+    _attachEvent(window, 'scroll', function () { showTopLink(); });checkBlind();</script> 
  
  			<div id="discuz_tips" style="display:none;"></div>
 			<script type="text/javascript">
@@ -654,4 +664,4 @@ GMT+8, 2014-12-17 09:42<span id="debuginfo">
 			if($('debuginfo')) {
 				$('debuginfo').innerHTML = '. This page is cached  at 09:42:14  .';
 			}
-			</script>
+</script>
