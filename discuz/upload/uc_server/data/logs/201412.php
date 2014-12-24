8b8482498e9aa85ae882225d1a7eaeaf04d2b827
ylmemail <ylmemail@163.com>
2014-12-24 17:04:39 +0800

    12

--- discuz/upload/uc_server/data/logs/201412.php
+++ discuz/upload/uc_server/data/logs/201412.php
@@ -1,2 +1,3 @@
 <?PHP exit;?>	UCenterAdministrator	127.0.0.1	1419408071	login	succeed
 <?PHP exit;?>	UCenterAdministrator	127.0.0.1	1419408205	app_add	appid=2; appname=bbs
+<?PHP exit;?>	UCenterAdministrator	127.0.0.1	1419410677	login	succeed
