525e2c66c636c44c075cdd3ca32d53cd55da8470
ylmemail <ylmemail@163.com>
2014-12-26 10:32:44 +0800

    32

--- Application/Runtime/Cache/Home/53271e64980e678c878c9b8710bd153f.php
+++ Application/Runtime/Cache/Home/53271e64980e678c878c9b8710bd153f.php
@@ -84,7 +84,21 @@
 	  	  </div>
 
       <div class="clear"></div>
-     <div class="bshare-custom icon-medium-plus"><a title="分享到QQ空间" class="bshare-qzone"></a><a title="分享到新浪微博" class="bshare-sinaminiblog"></a><a title="分享到人人网" class="bshare-renren"></a><a title="分享到腾讯微博" class="bshare-qqmb"></a><a title="分享到网易微博" class="bshare-neteasemb"></a><a title="更多平台" class="bshare-more bshare-more-icon more-style-addthis"></a><span class="BSHARE_COUNT bshare-share-count">0</span></div><script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/buttonLite.js#style=-1&amp;uuid=&amp;pophcol=2&amp;lang=zh"></script><script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/bshareC0.js"></script>
+	  <!--分享-->
+		<div class="bshare-custom icon-medium-plus">
+		<a title="分享到QQ空间" class="bshare-qzone"></a>
+		<a title="分享到新浪微博" class="bshare-sinaminiblog"></a>
+		<a title="分享到人人网" class="bshare-renren"></a>
+		<a title="分享到腾讯微博" class="bshare-qqmb"></a>
+		<a title="分享到网易微博" class="bshare-neteasemb"></a>
+		<a title="更多平台" class="bshare-more bshare-more-icon more-style-addthis"></a>
+		<span class="BSHARE_COUNT bshare-share-count">0</span>
+		</div>
+		<script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/buttonLite.js#style=-1&amp;uuid=&amp;pophcol=2&amp;lang=zh">
+		</script>
+		<script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/bshareC0.js">
+		</script>
+		<!--分享end-->
 	  <?php if(is_array($data)): foreach($data as $key=>$vo): ?><div class="review">
 	  <!-- 评论列表 -->
 			<h3 class="reviewtitle"><span class="fl"><?php echo ($vo["user"]); ?>
