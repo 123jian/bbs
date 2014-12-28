4b3e7c98909ceacb2cb3df2fed6e87d89d7a5ae6
zjbgit <zj_4315@163.com>
2014-12-28 19:35:54 +0800

    we

--- Application/Home/Controller/JiuyeController.class.php
+++ Application/Home/Controller/JiuyeController.class.php
@@ -53,28 +53,29 @@ class JiuyeController extends Controller {
         $Jiuye = M("bbs_jiuye");
         $student_name = $_POST['student_name'];
         $school_name = $_POST['school_name'];
-		include('/sphinxapi.php');
-		$sphinx = new \SphinxClient();
-		/* 谁用改谁IP*/$sphinx->SetServer("127.0.0.1",9312);
-		$sphinx->SetMatchMode(SPH_MATCH_ANY);
-		$result = $sphinx->query($student_name,"*");
+		//echo $student_name."123".$school_name;die;
+		//include('/sphinxapi.php');
+		//$sphinx = new \SphinxClient();
+		/* 谁用改谁IP$sphinx->SetServer("127.0.0.1",9312);*/
+		//$sphinx->SetMatchMode(SPH_MATCH_ANY);
+		//$result = $sphinx->query($student_name,"*");
 		//var_dump($result);die;
-		$key = array_keys($result['matches']);
+		//$key = array_keys($result['matches']);
 		//var_dump($result['matches']);
-		$ids = implode(',',$key);
+		//$ids = implode(',',$key);
 		$where = " where 1=1";
-		if(!empty($student_name)) {
-                $where .= " and id in($ids)";
+		if($student_name) {
+                $where .= " and student_name like '%$student_name%'";
           //  $map['student_name'] = array('like',"%{$student_name}%");
         }
-        if(!empty($school_name)) {
+        if($school_name) {
                 $where .= " and school_name like '%$school_name%'";
             ////$map['school_name'] = array('like',"%{$school_name}%");
         }
 		$sql="select * from bbs_jiuye" . $where;
 		//echo $sql;die;
 		$list = $Jiuye->query($sql);
-	//	$list = $Jiuye ->  where($map)-> select();
+		//$list = $Jiuye ->  where($map)-> select();
 		//var_dump($list);die;
         $Jiuye1 = M("jiuye1");
         $res = $Jiuye1->select();
