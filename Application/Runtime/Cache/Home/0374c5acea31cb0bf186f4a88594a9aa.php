876fbeb99a13f6b7212e70aa77f20f2fa088f006
zjbgit <zj_4315@163.com>
2014-12-29 09:27:21 +0800

    sdg

--- Application/Runtime/Cache/Home/0374c5acea31cb0bf186f4a88594a9aa.php
+++ Application/Runtime/Cache/Home/0374c5acea31cb0bf186f4a88594a9aa.php
@@ -56,7 +56,7 @@
 	<li nid="nav_life" width="15%"><a href="/discuz/upload/forum.php" id="nav_main" target="_blank">技术论坛</a></li>
 </ul>
 	</div>
-	</body>
+
 <div id="box">
 		    <div class="mbx">
 			<!-- Baidu share BEGIN -->
@@ -178,3 +178,7 @@
 	<p style="padding-top:10px"><a target="_blank" href="http://webscan.360.cn/index/checkwebsite/url/www.itcast.cn" style="overflow:hidden; margin-right:6px; padding:0;"><img width="124" height="47" src="http://www.itcast.cn/images/360anquan.png"></a><a target="_blank" href="http://t.knet.cn/index_new.jsp" style="overflow:hidden; margin-right:6px; padding:0;"><img width="124" height="47" src="http://www.itcast.cn/images/cx.png"></a><a style="padding:0;" href="http://www.bj.cyberpolice.cn/index.do" target="_blank"><img width="124" height="47" src="http://www.itcast.cn/images/jc.png"></a></p>
   </div>
 </div>
+
+
+
+<body>
