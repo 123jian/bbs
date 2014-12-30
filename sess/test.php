aaf949051345f9f8b3c458bfe5e1bf6c7ccd4d01
zjbgit <zj_4315@163.com>
2014-12-30 08:50:21 +0800

    ad

---
+++ sess/test.php
@@ -0,0 +1,8 @@
+<?php
+ini_set('session.save_handler','memcache');
+ini_set('session.save_path','127.0.0.1:11211');
+session_start();
+$hot=123;
+$_SESSION['hot']=$hot;
+var_dump(session_id());
+?>
---
+++ sess/test2.php
@@ -0,0 +1,7 @@
+<?php
+
+ini_set('session.save_handler','memcache');
+ini_set('session.save_path','192.168.1.219:11211');
+session_start();
+echo $_SESSION['hot'];
+?>
