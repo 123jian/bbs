fc8c28b79f46ba167b2116f1eaa73d582c1465f5
zjbgit <zj_4315@163.com>
2015-01-03 09:03:54 +0800

    wef

--- jiekou/jiekou.php
+++ jiekou/jiekou.php
@@ -10,7 +10,7 @@ if($str==$yao){
 	
 	$pdo=new PDO('mysql:dbname=bbs;host=192.168.1.217','root','');
 	$pdo->query('set names utf8');
-	$re=$pdo->query("select c_id,c_desc,c_time from bbs_category");
+	$re=$pdo->query("select c_id,c_name,c_desc,c_time from bbs_category");
 	$arr=$re->fetchall();
 	//echo $a."(".json_encode($arr).")";
 	echo @$_GET['callback']."(".json_encode($arr).")";
